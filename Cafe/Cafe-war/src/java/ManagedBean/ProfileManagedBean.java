9b5712fc7284b895a1ca2f2bb24349003148ef7d
zhixuan312 <zhangzhixuan312@gmail.com>
2016-10-15 20:15:18 +0800

    minor edit

--- Cafe/Cafe-war/src/java/ManagedBean/ProfileManagedBean.java
+++ Cafe/Cafe-war/src/java/ManagedBean/ProfileManagedBean.java
@@ -1,21 +1,15 @@
 package ManagedBean;
 
-import AccountManagement.AccountManagementRemote;
 import Entity.User_tb;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
-import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
 
 
 @Named(value = "profileManagedBean")
@@ -26,31 +20,60 @@ public class ProfileManagedBean implements Serializable {
     private LoginManagedBean loginManagedBean;
     
     private User_tb user;
+    private String editArea;
     
     
     public ProfileManagedBean() {
         user = new User_tb();
+        editArea = "";
     }
     
     @PostConstruct
     public void init() {
         user = loginManagedBean.getAccountManagementRemote().getUser();
     }
-
+    
+    public boolean showEditArea(){
+        return editArea.equals("name");
+    }
+    
+    public void updateUser(){
+        loginManagedBean.getAccountManagementRemote().updateUser(user);
+        loginManagedBean.getAccountManagementRemote().setUser(user);
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("profile.xhtml?faces-redirect=true");
+        } catch (IOException ex) {
+            Logger.getLogger(ScheduleManagementManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public String showHourlyPayment(){
+        return (user.getHourlyPayment()==0) ? "∞":"$"+user.getHourlyPayment();
+    }
+    
     public LoginManagedBean getLoginManagedBean() {
         return loginManagedBean;
     }
-
+    
     public void setLoginManagedBean(LoginManagedBean loginManagedBean) {
         this.loginManagedBean = loginManagedBean;
     }
-
+    
     public User_tb getUser() {
         return user;
     }
-
+    
     public void setUser(User_tb user) {
         this.user = user;
+    }
+    
+    public String getEditArea() {
+        return editArea;
+    }
+    
+    public void setEditArea(String editArea) {
+        this.editArea = editArea;
     }
     
 }
